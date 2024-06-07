9b53b6f89b828998687ade4342440dd52cd84815
renit4 <145850245+renit4@users.noreply.github.com>
2024-06-07 19:40:25 -0300

    .

---
+++ conexion.php
@@ -0,0 +1,4 @@
+<?php
+    $con = mysqli_coneccted("localhost","root","","bibliooteca");
+    
+?>
---
+++ prestamo/reg_prestamo.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrar Prestamos</title>
+</head>
+<body>
+    <h1><center>Registro de Prestamos</center></h1>
+    <center><a href="">Menu</a></center>
+    <br><br>
+    <form action="" method="post">
+        <table>
+            <tr>
+                <td></td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="text"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
